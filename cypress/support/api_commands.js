6ecdfdcd9b65bedebb66e771ec4352a184d051df
guipastl <guipastl@hotmail.com>
2025-01-16 01:29:23 -0300

    general baseUrl fix

--- cypress/support/api_commands.js
+++ cypress/support/api_commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('api_login', () => {
   cy.request({
     method: 'POST',
-    url: `${Cypress.env('api_server')}/api/access-tokens`,
+    url: '/api/access-tokens',
     body: {
       emailOrUsername: Cypress.env('user_name'),
       password: Cypress.env('user_password')
@@ -14,20 +14,20 @@ Cypress.Commands.add('api_getAllProjects', () => {
     .then((cookie) => {
       cy.request({
         method: 'GET',
-        url: `${Cypress.env('api_server')}/api/projects`,
+        url: '/api/projects',
         headers:  { Authorization: `Bearer ${cookie.value}` }
       })
     })
 })
 
-Cypress.Commands.add('api_deleteProjects', () => {
+Cypress.Commands.add( 'api_deleteProjects', () => {
   cy.getCookie('accessToken')
     .then((cookie) => {
       cy.api_getAllProjects()
         .then(res =>
           res.body.items.forEach(project => cy.request({
             method: 'DELETE',
-            url: `${Cypress.env('api_server')}/api/projects/${project.id}`,
+            url: `/api/projects/${project.id}`,
             headers: { Authorization: `Bearer ${cookie.value}` }
           }))
       )
@@ -39,7 +39,7 @@ Cypress.Commands.add('api_createProject', project => {
     .then((cookie) => {
       cy.request({
         method: 'POST',
-        url: `${Cypress.env('api_server')}/api/projects`,
+        url: '/api/projects',
         body: { name: `${project.name}`  },
         headers:  { Authorization: `Bearer ${cookie.value}` }
       })
